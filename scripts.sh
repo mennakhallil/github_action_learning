b1122fe61f15ce76539ed3b9dfe5fab3568440c3
mennakhallil <menah.khalill@gmail.com>
2026-07-27 15:39:09 +0300

    add cowsay output

--- scripts.sh
+++ scripts.sh
@@ -4,4 +4,4 @@ set -e
 touch "$GITHUB_WORKSPACE/cowsay.txt"
 sudo apt-get update
 sudo apt-get install -y cowsay >> "$GITHUB_WORKSPACE/cowsay.txt"
-cowsay "Hello from C3!" >> "$GITHUB_WORKSPACE/cowsay.txt"
+cowsay -f dragon "Hello from C3!" >> "$GITHUB_WORKSPACE/cowsay.txt"
