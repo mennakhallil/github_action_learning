2c08068888b45405a5eb3a9b1450126b144feb53
mennakhallil <menah.khalill@gmail.com>
2026-07-27 15:16:34 +0300

    add cowsay output

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+touch /github/workspace/cowsay.txt
 sudo apt-get update &&
 sudo apt-get install -y cowsay >> /github/workspace/cowsay.txt
 cowsay "Hello from C3!" >> /github/workspace/cowsay.txt
