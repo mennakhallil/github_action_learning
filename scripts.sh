bada54b8f937fa91066e677b2e78cde88a904e91
mennakhallil <menah.khalill@gmail.com>
2026-07-27 15:51:10 +0300

    fix: update file path for cowsay output to use relative path

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-touch "$GITHUB_WORKSPACE/cowsay.txt"
+touch "./cowsay.txt"
 sudo apt-get update
-sudo apt-get install -y cowsay >> "$GITHUB_WORKSPACE/cowsay.txt"
-cowsay -f dragon "Hello from C3!" >> "$GITHUB_WORKSPACE/cowsay.txt"
-cat "$GITHUB_WORKSPACE/cowsay.txt"
+sudo apt-get install -y cowsay >> "./cowsay.txt"
+cowsay -f dragon "Hello from C3!" >> "./cowsay.txt"
+cat "./cowsay.txt"
